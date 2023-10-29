f9070abb6742b899b762933c38eb0fdf871db9e1
ahmedomar <ahmedomarar365@gmail.com>
2023-10-29 23:04:56 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Greet from './lib/Greet.svelte'
+</script>
+
+<main class="container">
+
+  <div class="row">
+
+      <img src="/logo/zamanie.png" class="logo svelte" alt="Svelte Logo" />
+  </div>
+
+  <p>
+    Click on the Tauri, Vite, and Svelte logos to learn more.
+  </p>
+
+  <div class="row">
+  </div>
+
+
+</main>
+
+<style>
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+</style>
